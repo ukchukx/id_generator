a147aa057522f055f977411241e863c336c68ef0
Uk Chukundah <ukchukundah@gmail.com>
2017-06-10 18:28:30 +0100

    Simulate default args.

--- IDGenerator.java
+++ IDGenerator.java
@@ -16,12 +16,12 @@ public class IDGenerator {
    * @return integer
    */
   public static long generate(int typeCode) {
-    String typeCodeBits = padString(Integer.toUnsignedString(typeCode, 2), 8);
+    String typeCodeBits = padString(Integer.toUnsignedString(typeCode, 2));
     String tsBits = padString(Long.toUnsignedString(new Date().getTime(), 2), 48);
 
     String uuid = UUID.randomUUID().toString().split("-")[4];
     uuid = uuid.replace(uuid.substring(0, 10), "").trim(); // We only want last 8 bits
-    String uuidBits = padString(Long.toUnsignedString(Long.parseUnsignedLong(uuid, 16), 2), 8);
+    String uuidBits = padString(Long.toUnsignedString(Long.parseUnsignedLong(uuid, 16), 2));
 
     return Long.parseUnsignedLong(String.format("%s%s%s", typeCodeBits, tsBits, uuidBits), 2);
   }
@@ -59,5 +59,9 @@ public class IDGenerator {
     for(int p = 1; p <= (length - bitString.length()); ++p) pad += "0";
 
     return String.format("%s%s", pad, bitString);
+  }
+
+  public static String padString(String bitString) {
+    return padString(bitString, 8);
   }
 }
